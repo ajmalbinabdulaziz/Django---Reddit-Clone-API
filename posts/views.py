fc9b94eda0c03ce8ea3283f6b6ab3497b4e66d05
ajmalbinabdulaziz <ajmalbinabdulaziz@gmail.com>
2020-11-30 20:07:49 +0530

    Deleting Votes

--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from rest_framework import generics, permissions
+from rest_framework import generics, permissions, mixins, status
+from rest_framework.response import Response
 from rest_framework.exceptions import ValidationError
 from .models import Post, Vote
 from .serializers import PostSerializer, VoteSerializer
@@ -14,7 +15,7 @@ class PostList(generics.ListCreateAPIView):
         serializer.save(poster=self.request.user)
 
 
-class VoteCreate(generics.CreateAPIView):
+class VoteCreate(generics.CreateAPIView, mixins.DestroyModelMixin):
     serializer_class = VoteSerializer
     permission_classes = [permissions.IsAuthenticated]
 
@@ -28,3 +29,10 @@ class VoteCreate(generics.CreateAPIView):
             raise ValidationError("You've already voted for this post!")
         serializer.save(voter=self.request.user,
                         post=Post.objects.get(pk=self.kwargs['pk']))
+
+    def delete(self, request, *args, **kwargs):
+        if self.get_queryset():
+            self.get_queryset().delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        else:
+            raise ValidationError("You never voted for this post!")
